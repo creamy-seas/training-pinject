d105eaf262ae8daacffbe6f4133f7a7f7eefd94e
Ivan-Antonov <Ivan.Antonov.2015@live.rhul.ac.uk>
2020-04-09 09:42:26 +0800

    formatting

--- app/decorators/delay_decorator.py
+++ app/decorators/delay_decorator.py
@@ -2,17 +2,17 @@
 Example Decorator to delay the printing
 """
 
-#pylint: disable=C
+# pylint: disable=C
 from time import sleep
 import pinject
 
 
-class DelayDecorator():
+class DelayDecorator:
     @pinject.copy_args_to_public_fields
     def __init__(self, app_logger):
         pass
 
-    def run(self, function, input_text, seconds_to_delay):
-        self.app_logger.log(f'Delaying for {seconds_to_delay}')
+    def run(self, function, arguements, seconds_to_delay):
+        self.app_logger.log(f"Delaying for {seconds_to_delay}")
         sleep(seconds_to_delay)
-        function(input_text)
+        function(arguements)
